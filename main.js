117296d862191289f2b0ae2e192e9a6bd0d3458c
Tanja Karius <tanja.karius@googlemail.com>
2021-10-23 15:37:26 +0200

    Add checkbox automatically

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-const userInput = document.getElementById('userinput')
-const addButton = document.getElementById('addtask')
 const taskList = document.getElementById('tasks')
 
 // console.log(taskList)
 
 const addTask = () => {
+    const userInput = document.getElementById('userinput')
+    let checkBox = document.createElement('input'); //checkbox
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('unchecked')
     // console.log('User input is: ' + userInput.value)
     let task = document.createElement('li')
+    task.appendChild(checkBox)
     task.appendChild(document.createTextNode(userInput.value))
     taskList.appendChild(task)
     document.getElementById('userinput').value = ''
 }
+
+// Toggle checkbox
+/* 
+
+let checkBoxes = document.querySelectorAll('.unchecked').checked = false;
+
+let listItem = document.querySelector('input.unchecked')
+console.log() 
+
+*/
